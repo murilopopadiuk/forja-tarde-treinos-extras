bbd336b0e773a007132d48f876beb9e0f8d6abf4
murilopopadiuk <murilo.popadiuk@aluno.senai.br>
2025-10-30 16:46:46 -0300

    Add files via upload

---
+++ Contador/script.js
@@ -0,0 +1,22 @@
+function contar(){
+  const num = parseInt(document.getElementById('numeroinput').value)
+
+   if(isNaN(num)){
+    document.getElementById('result').textContent = 'informe um numero valido'
+ }
+
+var resultadoStr = ""
+
+
+if(num <= 10000){
+for(var cont = 1; cont <= num; cont++){  
+     resultadoStr = resultadoStr + cont + "<br>"
+
+}
+ document.getElementById('result').innerHTML = resultadoStr
+}else{
+    document.getElementById('result').textContent = 'Digite um numero menor que 10.000'
+}
+
+
+}
